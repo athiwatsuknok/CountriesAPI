ffae3670c28e157560f397e84f7b467bd665de9b
Athiwat Suknok <athiwatsuknok@Athiwats-MacBook-Pro.local>
2023-10-28 23:31:27 +0700

    Search Countries API

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="mx-auto w-full overflow-hidden border-b-[1px] border-slate-200">
+      <div className="flex  justify-between h-[60px] mx-auto items-center w-4/5 ">
+        <h1 className="font-medium text-lg">Where is the world ?</h1>
+        <h3>Hello World</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ types/index.ts
@@ -0,0 +1,24 @@
+export interface CountryData {
+  name: string;
+  topLevelDomain: string[];
+  alpha2Code: string;
+  alpha3Code: string;
+  callingCodes: string[];
+  capital: string;
+  altSpellings: string[];
+  subregion: string;
+  region: string;
+  population: number;
+  latlng: number[];
+  demonym: string;
+  area: number;
+  gini: number;
+  timezones: string[];
+  borders: string[];
+  nativeName: string;
+  numericCode: string;
+  flags: {
+    svg: string;
+    png: string;
+  };
+}
